6c6c2bbda713a1fe87af92ee08a6f3a7970e1590
Dante <bartosz.bk04@gmail.com>
2018-11-20 15:19:16 +0100

    DDA algorithm implementation

--- RayCastingSDL/Source.cpp
+++ RayCastingSDL/Source.cpp
@@ -1,18 +1,118 @@
 #include <iostream>
 #include <SDL.h>
-using namespace std;
+#define mapWidth 24
+#define mapHeight 24
+
+int map[mapWidth][mapHeight]=
+{
+	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+	{1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,0,0,3,0,0,0,1},
+	{1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+};
 
 int main(int argc, char * argv[])
 {
-	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+	//x and y start player position 
+	double posX = 22;
+	double posY = 12;
+	//intial direction vector
+	double dirX = -1;
+	double dirY = 0;
+	//the 2d raycaster version of camera plane
+	double planeX = 0;
+	double planeY = 0.66;
+	double time = 0;//time of current frame
+	double oldTime = 0;//time of previous frame
+	//TODO: screen and done function
+	while (true)
 	{
-		cout << "SDL initialization failed. SDL Error: " << SDL_GetError();
-	}
-	else
-	{
-		cout << "SDL initialization succeeded!";
-	}
+		for (int x = 0; x < w; ++x)
+		{
+			//calculate ray position and direction
+			double cameraX = 2 * x / double(w)-1; //x-coordinate in camera space
+			double rayDirX = dirX + planeX * cameraX;
+			double rayDirY = dirY + planeY * cameraX;
+			//which box of the map ae're in
+			int mapX = (int)posX;
+			int mapY = (int)posY;
+			//lenght of ray from current position to next x or y-side
+			double sideDistX;
+			double sideDistY;
+			//lenght of ray from one x or Y-side
+			double deltaDistX = std::abs(1 / rayDirX);
+			double deltaDistY = std::abs(1 / rayDirY);
+			double perpWallDist;
+			//what direction to step in x or y-direction (either +1 or -1)
+			int stepX;
+			int stepY;
 
-	cin.get();
+			int hit = 0; //was there a wall hit?
+			int side; //was a NS or EW  wall hit
+			//calculate step and initial sideDist
+			if (rayDirX < 0)
+			{
+				stepX = -1;
+				sideDistX = (posX - mapX)*deltaDistX;
+			}
+			else
+			{
+				stepX = 1;
+				sideDistX = (mapX + 1.0 - posX)*deltaDistX;
+			}
+			if (rayDirY < 0)
+			{
+				stepY = -1;
+				sideDistY = (posY - mapY)*deltaDistY;
+			}
+			else
+			{
+				stepY = 1;
+				sideDistY = (mapY + 1.0 - posY) * deltaDistY;
+			}
+			//perform DDA
+			while (hit == 0)
+			{
+				//jump to next map square, OR in x-dir, OR in y-dir
+				if (sideDistX < sideDistY)
+				{
+					sideDistX += deltaDistX;
+					mapX += stepX;
+					side = 0;
+				}
+				else
+				{
+					sideDistY += deltaDistY;
+					mapY += stepY;
+					side = 1;
+				}
+				//Check if ray has hit a wall
+				if (map[mapX][mapY] > 0)
+				{
+					hit = 1;
+				}
+			}
+		}
+	}
 	return 0;
 }
