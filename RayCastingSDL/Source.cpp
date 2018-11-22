5abe17e3d200e6b8bc2a1f1c9c47e4b871f3d3dd
Dante <bartosz.bk04@gmail.com>
2018-11-22 21:40:37 +0100

    Render random texture

--- RayCastingSDL/Source.cpp
+++ RayCastingSDL/Source.cpp
@@ -5,7 +5,10 @@
 
 #include "quickcg.h"
 using namespace QuickCG;
-
+#define screenWidth 800
+#define screenHeight 600
+#define texWidth 64
+#define texHeight 64
 #define mapWidth 24
 #define mapHeight 24
 using namespace QuickCG;
@@ -45,44 +48,70 @@ int map[mapWidth][mapHeight]=
 	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
 };
 
+Uint32 buffer[screenHeight][screenWidth];
+
 int main(int /*argc*/, char */*argv*/[])
 {
-	//x and y start player position 
-	double posX = 22;
-	double posY = 12;
-	//intial direction vector
-	double dirX = -1;
-	double dirY = 0;
-	//the 2d raycaster version of camera plane
-	double planeX = 0;
-	double planeY = 0.66;
-	double time = 0;//time of current frame
-	double oldTime = 0;//time of previous frame
-	screen(1280, 720,0, "RayCasting");
+	double posX = 22.0, posY = 11.5;  //x and y start position
+	double dirX = -1.0, dirY = 0.0; //initial direction vector
+	double planeX = 0.0, planeY = 0.66; //the 2d raycaster version of camera plane
+
+	double time = 0; //time of current frame
+	double oldTime = 0; //time of previous frame
+
+	std::vector<Uint32> texture[8];
+	for (int i = 0; i < 8; i++) texture[i].resize(texWidth * texHeight);
+
+	screen(screenWidth, screenHeight, 0, "Raycaster");
+
+	//generate some textures
+	for (int x = 0; x < texWidth; x++)
+		for (int y = 0; y < texHeight; y++)
+		{
+			int xorcolor = (x * 256 / texWidth) ^ (y * 256 / texHeight);
+			//int xcolor = x * 256 / texWidth;
+			int ycolor = y * 256 / texHeight;
+			int xycolor = y * 128 / texHeight + x * 128 / texWidth;
+			texture[0][texWidth * y + x] = 65536 * 254 * (x != y && x != texWidth - y); //flat red texture with black cross
+			texture[1][texWidth * y + x] = xycolor + 256 * xycolor + 65536 * xycolor; //sloped greyscale
+			texture[2][texWidth * y + x] = 256 * xycolor + 65536 * xycolor; //sloped yellow gradient
+			texture[3][texWidth * y + x] = xorcolor + 256 * xorcolor + 65536 * xorcolor; //xor greyscale
+			texture[4][texWidth * y + x] = 256 * xorcolor; //xor green
+			texture[5][texWidth * y + x] = 65536 * 192 * (x % 16 && y % 16); //red bricks
+			texture[6][texWidth * y + x] = 65536 * ycolor; //red gradient
+			texture[7][texWidth * y + x] = 128 + 256 * 128 + 65536 * 128; //flat grey texture
+		}
+
+	//start the main loop
 	while (!done())
 	{
-		for (int x = 0; x < w; ++x)
+		for (int x = 0; x < w; x++)
 		{
 			//calculate ray position and direction
 			double cameraX = 2 * x / double(w) - 1; //x-coordinate in camera space
 			double rayDirX = dirX + planeX * cameraX;
 			double rayDirY = dirY + planeY * cameraX;
+
 			//which box of the map we're in
 			int mapX = int(posX);
 			int mapY = int(posY);
+
 			//length of ray from current position to next x or y-side
 			double sideDistX;
 			double sideDistY;
+
 			//length of ray from one x or y-side to next x or y-side
 			double deltaDistX = std::abs(1 / rayDirX);
 			double deltaDistY = std::abs(1 / rayDirY);
 			double perpWallDist;
+
 			//what direction to step in x or y-direction (either +1 or -1)
 			int stepX;
 			int stepY;
 
 			int hit = 0; //was there a wall hit?
 			int side; //was a NS or a EW wall hit?
+
 			//calculate step and initial sideDist
 			if (rayDirX < 0)
 			{
@@ -123,7 +152,8 @@ int main(int /*argc*/, char */*argv*/[])
 				//Check if ray has hit a wall
 				if (map[mapX][mapY] > 0) hit = 1;
 			}
-			//Calculate distance projected on camera direction (Euclidean distance will give fisheye effect!)
+
+			//Calculate distance of perpendicular ray (Euclidean distance will give fisheye effect!)
 			if (side == 0) perpWallDist = (mapX - posX + (1 - stepX) / 2) / rayDirX;
 			else           perpWallDist = (mapY - posY + (1 - stepY) / 2) / rayDirY;
 
@@ -132,37 +162,49 @@ int main(int /*argc*/, char */*argv*/[])
 
 			//calculate lowest and highest pixel to fill in current stripe
 			int drawStart = -lineHeight / 2 + h / 2;
-			if (drawStart < 0)drawStart = 0;
+			if (drawStart < 0) drawStart = 0;
 			int drawEnd = lineHeight / 2 + h / 2;
-			if (drawEnd >= h)drawEnd = h - 1;
+			if (drawEnd >= h) drawEnd = h - 1;
 
-			//choose wall color
-			ColorRGB color;
-			switch (map[mapX][mapY])
+			//texturing calculations
+			int texNum = map[mapX][mapY] - 1; //1 subtracted from it so that texture 0 can be used!
+
+			//calculate value of wallX
+			double wallX; //where exactly the wall was hit
+			if (side == 0) wallX = posY + perpWallDist * rayDirY;
+			else           wallX = posX + perpWallDist * rayDirX;
+			wallX -= floor((wallX));
+
+			//x coordinate on the texture
+			int texX = int(wallX * double(texWidth));
+			if (side == 0 && rayDirX > 0) texX = texWidth - texX - 1;
+			if (side == 1 && rayDirY < 0) texX = texWidth - texX - 1;
+
+			for (int y = drawStart; y < drawEnd; y++)
 			{
-			case 1:  color = RGB_Red;  break; //red
-			case 2:  color = RGB_Green;  break; //green
-			case 3:  color = RGB_Blue;   break; //blue
-			case 4:  color = RGB_White;  break; //white
-			default: color = RGB_Yellow; break; //yellow
+				int d = y * 256 - h * 128 + lineHeight * 128;  //256 and 128 factors to avoid floats
+				// TODO: avoid the division to speed this up
+				int texY = ((d * texHeight) / lineHeight) / 256;
+				Uint32 color = texture[texNum][texHeight * texY + texX];
+				//make color darker for y-sides: R, G and B byte each divided through two with a "shift" and an "and"
+				if (side == 1) color = (color >> 1) & 8355711;
+				buffer[y][x] = color;
 			}
-
-			//give x and y sides different brightness
-			if (side == 1) { color = color / 2; }
-
-			//draw the pixels of the stripe as a vertical line
-			verLine(x, drawStart, drawEnd, color);
 		}
+
+		drawBuffer(buffer[0]);
+		for (int x = 0; x < w; x++) for (int y = 0; y < h; y++) buffer[y][x] = 0; //clear the buffer instead of cls()
 		//timing for input and FPS counter
 		oldTime = time;
 		time = getTicks();
-		double frameTime = (time - oldTime) / 1000.0;
-		print(1.0 / frameTime);
+		double frameTime = (time - oldTime) / 1000.0; //frametime is the time this frame has taken, in seconds
+		print(1.0 / frameTime); //FPS counter
 		redraw();
-		cls();
-		//speed modifires
-		double moveSpeed = frameTime * 5.0;
-		double rotSpeed = frameTime * 3.0;
+
+		//speed modifiers
+		double moveSpeed = frameTime * 5.0; //the constant value is in squares/second
+		double rotSpeed = frameTime * 3.0; //the constant value is in radians/second
+
 		readKeys();
 		//move forward if no wall in front of you
 		if (keyDown(SDLK_UP))
